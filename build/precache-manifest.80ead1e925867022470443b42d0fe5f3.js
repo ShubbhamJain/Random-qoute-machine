8b1e14003b02f0c18fdd8553c88e6f1992ac0a16
ShubbhamJain <shubham.jain368@gmail.com>
2020-05-26 21:18:43 +0530

    commiting build

---
+++ build/precache-manifest.80ead1e925867022470443b42d0fe5f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2acd91a8e08c39691724ce03041e5d21",
+    "url": "./index.html"
+  },
+  {
+    "revision": "784e09c5f2f4403fb0c1",
+    "url": "./static/css/main.b73aae28.chunk.css"
+  },
+  {
+    "revision": "be1a5d2ad73a97eee2b1",
+    "url": "./static/js/2.9cbcea58.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.9cbcea58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "784e09c5f2f4403fb0c1",
+    "url": "./static/js/main.00667bf7.chunk.js"
+  },
+  {
+    "revision": "639c4c25fcad769e5140",
+    "url": "./static/js/runtime-main.615a4d16.js"
+  }
+]);
